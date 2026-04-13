c2cc905586fbc327b09ed4c219d04ae240add53c
LaurentMT <laurentmt145@gmail.com>
2026-04-13 14:56:01 +0200

    Implement dynamic display of app version

--- pages/components/footer/footer.js
+++ pages/components/footer/footer.js
@@ -1,12 +1,19 @@
 const footerScript = {
 
   initComponent: () => {
+    // Initializes event handlers
     document.querySelector('#credits-link').addEventListener(
       'click', (e) => {
         e.preventDefault();
         goToPage('#credits');
       }
     );
+    // Displays version
+    const version = sessionStorage.getItem('appVersion')
+      ? sessionStorage.getItem('appVersion')
+      : '0.1-a.1';
+    document.querySelector('#version-span').textContent = `VERSION ${version}`;
+    
   },
 
   /*
